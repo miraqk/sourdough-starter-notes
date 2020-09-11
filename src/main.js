f18b26c70211623713b50f7e4d523e5c88e7d02b
Mira Kim <mira.kim@edelman.com>
2020-09-11 00:13:46 -0500

    Changing the way webfonts are imported to better match Gridsome recommendations.

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@
 import 'sanitize.css'
 import '~/styles/index.styl'
 
-
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {
+  head.link.push({
+  	rel: 'stylesheet',
+  	href: 'https://fonts.googleapis.com/css2?family=Gaegu:wght@400&family=Nunito:ital,wght@0,200;0,300;0,700;1,300;1,700&display=swap'
+  })
+
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
